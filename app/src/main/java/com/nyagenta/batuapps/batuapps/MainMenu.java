5104c2b910acb9823d31c82ca10dec6465c4175b
Aisy Rozsidhy <aisymuhammad@gmail.com>
2017-02-05 20:16:12 +0700

    merubah menu dan menambah image slider

--- app/src/main/java/com/nyagenta/batuapps/batuapps/MainMenu.java
+++ app/src/main/java/com/nyagenta/batuapps/batuapps/MainMenu.java
@@ -1,6 +1,7 @@
 package com.nyagenta.batuapps.batuapps;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,18 +13,88 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+//JSON
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+//memanggil library slider secara online
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
+import java.util.HashMap;
+
 
 public class MainMenu extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
 
+    //variabel slideLayout
+    private SliderLayout sliderLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        //memanggil view
         setContentView(R.layout.activity_main_menu);
+
+        // ==============================Image Slider ==============================================
+
+        sliderLayout = (SliderLayout)findViewById(R.id.slider);
+
+        //ambil gambar dari file online
+        HashMap<String,String> url_maps = new HashMap<String, String>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+        //ambil gambar dari file lokal
+        //HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
+        //file_maps.put("Hannibal",R.drawable.hannibal);
+        //file_maps.put("Big Bang Theory",R.drawable.bigbang);
+        //file_maps.put("House of Cards",R.drawable.house);
+        //file_maps.put("Game of Thrones", R.drawable.game_of_thrones);
+
+        //menampilkan gambar yang sudah di set
+        for(String name : url_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(MainMenu.this);
+            textSliderView.description(name)
+                    .image(url_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle().putString("extra", name);
+            sliderLayout.addSlider(textSliderView);
+        }
+
+        sliderLayout.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        sliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        sliderLayout.setCustomAnimation(new DescriptionAnimation());
+        sliderLayout.setDuration(7000); //mengatur durasi tampil
+        sliderLayout.addOnPageChangeListener(this);
+
+        //============================ ListView Berita =============================================
+
+
+
+        //============================ Toolbar =====================================================
+
+        //memanggil toolbar dan kemudia set barnya
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        //=========================== Floating Button ==============================================
+
+        //membuat floating button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +104,9 @@ public class MainMenu extends AppCompatActivity
             }
         });
 
+        //=========================== Sidebar ======================================================
+
+        //memanggil sidebar
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -42,6 +116,31 @@ public class MainMenu extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+
+
+    //============================== fungsi Slider ================================================
+    @Override
+    protected void onStop(){
+        sliderLayout.stopAutoCycle();
+        super.onStop();
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(this,slider.getBundle().get("extra") + "",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels){}
+
+    @Override
+    public void onPageSelected(int position){
+        Log.d("Slider Demo", "Page Changed:"+position);
+    }
+
+    public void onPageScrollStateChanged(int state){}
+
+    // ====================================== Fungsi Side Bar ======================================
 
     @Override
     public void onBackPressed() {
@@ -76,24 +175,29 @@ public class MainMenu extends AppCompatActivity
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
+
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_event) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_galeri) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_video) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_wisata) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_kuliner) {
 
         }
+//        else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
